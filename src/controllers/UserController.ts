ed6e2a4eefc2db50195763fea71b075f80ed4ecf
Paulo Mariano <paulomariano2501@gmail.com>
2022-07-21 01:34:17 -0300

    list, find, create, update and delete methods added

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,19 +1,65 @@
-import {Request, Response} from 'express'
+import { Request, Response } from 'express'
 import User from '../schemas/User'
 
 
 class UserController {
-  public async index (req: Request,res : Response): Promise<Response> {
-   const users = await User.find()
 
-   return res.json(users)
+  public async listUser(req: Request, res: Response): Promise<Response> {
+
+    try {
+      const user = await User.find()
+      return res.status(200).json(user)
+
+    } catch (err) {
+      return res.status(500).json({ message: "Error" })
+    }
   }
 
-public async store (req:Request, res:Response): Promise<Response> {
-  const user = await User.create(req.body)
+  public async getUser(req: Request, res: Response): Promise<Response> {
 
-  return res.json(user)
-}
+    try {
+      const user = await User.findById(req.params.id)
+      return res.status(200).json(user)
+
+    } catch (err) {
+      return res.status(500).json({ message: "Error" })
+    }
+  }
+
+  public async createUser(req: Request, res: Response): Promise<Response> {
+
+    try {
+      const user = await User.create(req.body)
+      return res.status(200).json(user)
+
+    } catch (err) {
+      return res.status(500).json({ message: "Error" })
+    }
+  }
+
+  public async updateUser(req: Request, res: Response): Promise<Response> {
+
+    try {
+      const user = await User.findByIdAndUpdate((req.params.id), req.body)
+      return res.status(200).json(user)
+
+    } catch (err) {
+      return res.status(500).json({ message: "Error" })
+    }
+  }
+
+  public async deleteUser(req: Request, res: Response): Promise<Response> {
+
+    try {
+      const user = await User.findByIdAndDelete(req.params.id)
+      return res.status(200).json(user)
+
+    } catch (err) {
+      return res.status(500).json({ message: "Error" })
+    }
+  }
+
+
 
 }
 
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,9 @@ import UserController from "./controllers/UserController";
 
 const routes = Router()
 
-routes.get('/users', UserController.index)
-routes.post('/users', UserController.store)
-
+routes.get('/users', UserController.listUser)
+routes.get('/users/:id', UserController.getUser)
+routes.post('/create', UserController.createUser)
+routes.put('/update/:id', UserController.updateUser)
+routes.delete('/delete/:id', UserController.deleteUser)
 export default routes
